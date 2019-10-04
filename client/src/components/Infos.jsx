f4eccb2b603ef15b9cb42d0e7d2e8467eba61e90
niraja426 <nrjshk@gmail.com>
2019-10-04 11:57:56 +0200

    info.jsxx working

--- client/src/components/Infos.jsx
+++ client/src/components/Infos.jsx
@@ -6,13 +6,28 @@ function Infos(props) {
   return (
 
     <div className="Infos">
-      <h1>Le QoQo</h1>
+      <h2 class="infoHeading">Le QoQo</h2>
+      <p className="desx">A guide to find events of your interest in France and the good plans for your outings and leisures with someone of similar interest!!!</p>
 
-      <div>An original idea by Setti Rais Ali</div>
+      <div className="setti">
+        <p>An original idea by <strong>Setti Rais Ali</strong> </p>
+      </div>
 
       <div className="dev_team">
-        <div className="Niraja">My name is Niraja, Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus minus assumenda voluptatem harum in dicta. Quisquam voluptates soluta deserunt aperiam, quo repellat. Temporibus aperiam nobis corporis doloribus tenetur consequuntur doloremque!</div>
-        <div className="Antonin">My Name is Antonin, Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure nihil cupiditate delectus quo fuga quod rerum perspiciatis nesciunt ad suscipit, explicabo ullam dolorem voluptate, doloremque corrupti? Recusandae unde omnis alias.</div>
+        <h3 className="devtt">The DEV TEaM</h3>
+        <div className="Antonin">
+
+          <i className="fas fa-user-ninja ninja-icon"></i>
+
+          <p> <strong>Antonin</strong> , Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure nihil cupiditate delectus quo fuga quod rerum perspiciatis nesciunt ad suscipit, explicabo ullam dolorem voluptate, doloremque corrupti? Recusandae unde omnis alias</p>
+        </div>
+
+
+        <div className="Niraja">
+          <i className="fas fa-user-ninja ninja-icon"></i>
+          <p><strong>Niraja</strong>, Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus minus assumenda voluptatem harum in dicta. Quisquam voluptates soluta deserunt aperiam, quo repellat. Temporibus aperiam nobis corporis doloribus tenetur consequuntur doloremque!</p>
+        </div>
+
       </div>
 
       <NavBar />
