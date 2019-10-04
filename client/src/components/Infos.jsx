07cbc26438bd20ecce782a5403ddce31fd33338c
niraja426 <nrjshk@gmail.com>
2019-10-04 09:58:18 +0200

    nothing changed

--- client/src/components/Infos.jsx
+++ client/src/components/Infos.jsx
@@ -2,20 +2,20 @@ import React from 'react'
 import NavBar from './NavBar'
 
 function Infos(props) {
-  
+
   return (
-    
+
     <div className="Infos">
       <div>An original idea by Setti Rais Ali</div>
-    
+
       <div className="dev_team">
         <div className="Niraja">My name is Niraja, Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus minus assumenda voluptatem harum in dicta. Quisquam voluptates soluta deserunt aperiam, quo repellat. Temporibus aperiam nobis corporis doloribus tenetur consequuntur doloremque!</div>
         <div className="Antonin">My Name is Antonin, Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure nihil cupiditate delectus quo fuga quod rerum perspiciatis nesciunt ad suscipit, explicabo ullam dolorem voluptate, doloremque corrupti? Recusandae unde omnis alias.</div>
       </div>
 
       <NavBar />
-      </div>
-    
+    </div>
+
   )
 
 }
